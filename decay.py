c610f68318439f2026be3593cd0219fb93944aab
Subhranil Sarkar <57196419+subhranil2605@users.noreply.github.com>
2022-06-23 18:04:38 +0530

    dfsd

--- decay.py
+++ decay.py
@@ -4,7 +4,8 @@ from matplotlib import animation
 
 import numpy as np
 
-# data generator 
+
+# data generator
 def gen_data():
     for cnt in itertools.count():
         t = cnt / 10
@@ -18,6 +19,7 @@ ax.grid()
 x_data, y_data = [], []
 
 
+# init function, it will be called once only
 def init():
     ax.set_xlim(-1.1, 1.1)
     ax.set_ylim(-2, 2.1)
@@ -29,21 +31,27 @@ def init():
     return line,
 
 
+# main function
 def run(data):
-    t, y = data
+    t, y = data  # data from gen_data() function
     x_data.append(t)
     y_data.append(y)
 
     x_min, x_max = ax.get_xlim()
 
+    # if the current x values same to the boundary of the x lime
     if t >= x_max:
+        # x_lim gets updated
         ax.set_xlim(x_min, 2 * x_max)
+        # drawing the new figure as the x_lim updated
         ax.figure.canvas.draw()
 
+    # setting the new data
     line.set_data(x_data, y_data)
     return line,
 
 
+# animation
 ani = animation.FuncAnimation(fig, run, gen_data, interval=5, init_func=init)
 
 plt.show()
